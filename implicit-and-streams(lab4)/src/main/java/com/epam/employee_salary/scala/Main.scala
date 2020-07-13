3448cb2d5efdef7ad5731f01d0d93eec5545023a
Sanshon1 <sanshon1@gmail.com>
2020-07-13 16:33:06 +0300

    Java Employee seniority calculations

--- implicit-and-streams(lab4)/src/main/java/com/epam/employee_salary/scala/Main.scala
+++ implicit-and-streams(lab4)/src/main/java/com/epam/employee_salary/scala/Main.scala
@@ -4,6 +4,24 @@ object Main {
   def main(args: Array[String]): Unit = {
 
     var employees:List[Employee] = List.tabulate(15)(n => Employee(salary = 10000+n*1500))
-    println(employees)
+
+    employees.map(emp => (SENIORITY.getSeniority(emp), emp))
+      .groupBy(_._1)
+      .mapValues(_.unzip._2)
+      .foreach(println)
+
+    employees.map(emp => (SENIORITY.getSeniority(emp), emp))
+      .groupBy(_._1)
+      .mapValues(_.unzip._2)
+      .map(tuple1 => (tuple1._1, tuple1._2.size))
+      .foreach(println)
+
+    employees.map(emp => (SENIORITY.getSeniority(emp), emp))
+      .groupBy(_._1)
+      .mapValues(_.unzip._2)
+      .map(tuple1 => (tuple1._1, tuple1._2.maxBy(_.salary)))
+      .foreach(println)
+
+
   }
 }
--- implicit-and-streams(lab4)/src/main/java/com/epam/employee_salary/scala/Seniority.scala
+++ implicit-and-streams(lab4)/src/main/java/com/epam/employee_salary/scala/Seniority.scala
@@ -3,5 +3,9 @@ package com.epam.employee_salary.scala
 object SENIORITY extends Enumeration {
   val JUNIOR, MIDDLE, SENIOR = Value
 
-  def getSeniority(employee: Employee):Unit = {}
+  def getSeniority(employee: Employee):Value = employee match {
+    case e: Employee if e.salary > 21000 => SENIORITY.SENIOR
+    case e: Employee if e.salary > 14000 => SENIORITY.MIDDLE
+    case e: Employee => SENIORITY.JUNIOR
+  }
 }
